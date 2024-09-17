01119acde8fd385194933b00e35fb41268ab43aa
mati0ri <mateo.iori223@gmail.com>
2024-09-17 10:51:23 +0200

    clean project, add test, add button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,36 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
-</template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      
+      <button @click="showMessage">Afficher Message</button>
+      <h1 v-if="show">Hello World</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        show: false
+      };
+    },
+    methods: {
+      showMessage() {
+        this.show = true;
+  
+        setTimeout(() => {
+          this.show = false;
+        }, 2000);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
   }
-}
-</style>
+  </style>
+  
--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,19 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+import { nextTick } from 'vue';
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
-
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe('HelloWorld.vue', () => {
+  it('should have a message that is a string', async () => {
+    const wrapper = mount(HelloWorld);
+    
+    // Simule un clic sur le bouton
+    wrapper.find('button').trigger('click');
+    
+    // Attends que le DOM se mette à jour
+    await nextTick();
+    
+    // Vérifie que l'élément <h1> contient bien le texte "Hello World"
+    expect(wrapper.find('h1').text()).toBe('Hello World');
+  });
+});
